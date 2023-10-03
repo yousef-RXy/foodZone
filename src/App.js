37e0952fbda18e73fcff754c2c98a287d066a5ca
Yousef Na <72698881+yousef-RXy@users.noreply.github.com>
2023-10-03 13:12:00 +0300

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h2>Let's gest started!</h2>
+		</div>
+	);
 }
 
 export default App;
