890326a8f7bb9cbffa2fa4ac8d1c4b01f7e0275e
Yousef Na <72698881+yousef-RXy@users.noreply.github.com>
2023-10-04 07:00:00 +0300

    Start at Cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,43 @@
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+	// const cartItems = props.items.map((item) => (
+	// 	<CartItem
+	// 		key={item.id}
+	// 		name={item.name}
+	// 		price={item.price}
+	// 		amount={item.amount}
+	// 		onRemove=""
+	// 		onAdd=""
+	// 	/>
+	// ));
+	const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 22.99 }].map(
+		(item) => (
+			<CartItem
+				key={item.id}
+				name={item.name}
+				price={item.price}
+				amount={item.amount}
+				onRemove=""
+				onAdd=""
+			/>
+		)
+	);
+
+	return (
+		<div>
+			{cartItems}
+			<div className={classes.total}>
+				<span>Total Amount</span>
+				<span>45.98</span>
+			</div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+		</>
+	);
+};
+
+export default Cart;
